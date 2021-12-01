7c52a8e65b8afd5376c9113e702a8c87b273b194
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-12-01 12:27:40 +0100

    ya

--- asscoiatieve arrays.php
+++ asscoiatieve arrays.php
@@ -1,22 +1,43 @@
 <?php
 
-echo "Agenda Les" ;
+echo "agenda les";
+echo "<br>";
 
-$hoofdstad1 =[];
-$hoofdstad1[] ="amsterdam";
-$hoofdstad1[] ="nederland";
-$hoofdstad1[] ="70000";
+$var1 = [];
+$var1[] = "Amsterdam"; 
+$var1[] = "Nederland"; 
+$var1[] = "700000"; 
+
+$hoofdstad0 = [
+    "Amsterdam",
+    "Nederland",
+    700000
+];
+//echo $hoofdstad0[2];
+//echo "<br>";
 $hoofdstad1 = [
-            "maan" => "amsterdam",
-            "land" =>"nederland",
-            "inwoners" =>70000];
+    "naam" => "Amsterdam",
+    "land" => "Nederland",
+    "inwoners" => 700000
+];
+//echo "<br>";
 $hoofdstad2 = [
-            "maan" => "parijs",
-            "land" =>"frankrijk",
-            "inwoners" => 70000];
-echo "<br>";
-print_r($hoofdstad1);
-echo "<br>";
-print_r($hoofdstad2);
+    "naam" => "Parijs",
+    "land" => "Frankrijk",
+    "inwoners" => 2000000
+];
+//echo $hoofdstad1["inwoners"];
 
-?>
+$hoofdsteden = [];
+$hoofdsteden[] = $hoofdstad1;
+$hoofdsteden[] = $hoofdstad2;
+
+//echo "<br>";
+//print_r ($hoofdsteden);
+echo "<br>";
+$json = json_encode($hoofdsteden);
+file_put_contents("data.txt", $json);
+
+$json2 = file_get_contents("data.txt");
+$hoofdsteden2 = json_decode($json2, true);
+print_r ($hoofdsteden2);
