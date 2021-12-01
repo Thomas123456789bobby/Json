d837ef50f891726db13d3c84fe7c9a9cc7f658e7
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-12-01 11:27:11 +0100

    ja

---
+++ asscoiatieve arrays.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Agenda Les" ;
+
+$hoofdstad1 =[];
+$hoofdstad1[] ="amsterdam";
+$hoofdstad1[] ="nederland";
+$hoofdstad1[] ="70000";
+$hoofdstad1 = [
+            "maan" => "amsterdam",
+            "land" =>"nederland",
+            "inwoners" =>70000];
+$hoofdstad2 = [
+            "maan" => "parijs",
+            "land" =>"frankrijk",
+            "inwoners" => 70000];
+echo "<br>";
+print_r($hoofdstad1);
+echo "<br>";
+print_r($hoofdstad2);
+
+?>
